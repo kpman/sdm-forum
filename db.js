b2c778e5d229a3549249fe1b071e96d61e703f80
kpman <s92f002@hotmail.com>
2014-10-23 00:48:25 +0800

    change DB export setting

--- db.js
+++ db.js
@@ -1,11 +1,31 @@
 var config = require('./config');
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var conn = {};
+conn.connection = mysql.createConnection({
   host     : config.host,
   user     : config.user,
   password : config.password,
   database : config.database
 });
 
-module.exports = connection;
+conn.handleDisconnect = function () {
+  conn.connection.connect(function(err) {
+    if (err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(conn.handleDisconnect, 2000);
+    }
+  });
+  conn.connection.on('error', function(err) {
+    console.log('db error', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      conn.handleDisconnect();
+    } else {                                 
+      throw err;
+    }
+  });
+}
+
+conn.handleDisconnect();
+
+module.exports = conn;
